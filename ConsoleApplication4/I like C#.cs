665ff0aec133b722cc8c6de17d2ec8dea3d47e87
kobo131 <jonas-wolff@gmx.de>
2014-09-14 14:57:58 +0200

    0.1
    Schinken

---
+++ ConsoleApplication4/I like C#.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zahl;
+            while (true)
+            {
+                Console.WriteLine("I like c#");
+            }
+        }
+    }
+}
